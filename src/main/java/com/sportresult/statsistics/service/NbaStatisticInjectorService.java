3f3062fa36fe3fe5357f9f444881d23625d01ff9
ludovicgarnier1979 <ludovicgarnier1979@gmail.com>
2025-06-25 09:36:32 +0200

    game_stats_per_year_inject

--- src/main/java/com/sportresult/statsistics/service/NbaStatisticInjectorService.java
+++ src/main/java/com/sportresult/statsistics/service/NbaStatisticInjectorService.java
@@ -3,6 +3,7 @@ package com.sportresult.statsistics.service;
 import com.sportresult.client.response.statistics.game.GameStatsData;
 import com.sportresult.client.response.statistics.game.GameStatsResponse;
 import com.sportresult.client.response.statistics.game.TeamStatsInGameData;
+import com.sportresult.game.dto.NbaGameDto;
 import com.sportresult.game.entity.NbaGameEntity;
 import com.sportresult.game.service.NbaGameInjectorService;
 import com.sportresult.statsistics.dto.NbaGameStatisticsDto;
@@ -18,8 +19,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -34,7 +33,7 @@ public class NbaStatisticInjectorService {
 
     private final NbaTeamInjectorService nbaTeamInjectorService;
 
-    public NbaGameStatisticsDto injectGameStatistic(Long nbaGameEntityOldId, GameStatsResponse response) {
+    public NbaGameStatisticsDto injectTeamGameStatistic(Long nbaGameEntityOldId, GameStatsResponse response) {
         if (response == null || response.getResponse().isEmpty()) {
             log.error("No game statistic to inject - empty response");
             return NbaGameStatisticsDto.builder().build();
@@ -62,11 +61,7 @@ public class NbaStatisticInjectorService {
         NbaTeamGameStatisticsEntity homeNbaTeamGameStatisticsEntity = buildNbaTeamGameStatisticEntity(homeNbaTeam, homeStatistics);
         nbaTeamGameStatisticInjectorRepository.save(homeNbaTeamGameStatisticsEntity);
 
-        NbaGameStatisticsEntity nbaGameStatisticsEntity = NbaGameStatisticsEntity.builder()
-                .nbaGameEntity(nbaGameEntity)
-                .homeTeamStatisticsEntity(homeNbaTeamGameStatisticsEntity)
-                .visitorTeamStatisticsEntity(visitorNbaTeamGameStatisticsEntity)
-                .build();
+        NbaGameStatisticsEntity nbaGameStatisticsEntity = NbaGameStatisticsEntity.builder().nbaGameEntity(nbaGameEntity).homeTeamStatisticsEntity(homeNbaTeamGameStatisticsEntity).visitorTeamStatisticsEntity(visitorNbaTeamGameStatisticsEntity).build();
 
         nbaGameStatisticInjectorRepository.saveAndFlush(nbaGameStatisticsEntity);
 
@@ -74,34 +69,23 @@ public class NbaStatisticInjectorService {
     }
 
     private NbaTeamGameStatisticsEntity buildNbaTeamGameStatisticEntity(NbaTeamEntity nbaTeam, TeamStatsInGameData teamStatsInGameData) {
-        return NbaTeamGameStatisticsEntity.builder()
-                .team(nbaTeam)
-                .fga(teamStatsInGameData.getFga())
-                .fgm(teamStatsInGameData.getFgm())
-                .fgp(teamStatsInGameData.getFgp())
-                .fta(teamStatsInGameData.getFta())
-                .ftm(teamStatsInGameData.getFtm())
-                .ftp(teamStatsInGameData.getFtp())
-                .min(teamStatsInGameData.getMin())
-                .assists(teamStatsInGameData.getAssists())
-                .biggestLead(teamStatsInGameData.getBiggestLead())
-                .fastBreakPoints(teamStatsInGameData.getFastBreakPoints())
-                .blocks(teamStatsInGameData.getBlocks())
-                .defReb(teamStatsInGameData.getDefReb())
-                .offReb(teamStatsInGameData.getOffReb())
-                .tpa(teamStatsInGameData.getTpa())
-                .tpm(teamStatsInGameData.getTpm())
-                .tpp(teamStatsInGameData.getTpp())
-                .pFouls(teamStatsInGameData.getPFouls())
-                .longestRun(teamStatsInGameData.getLongestRun())
-                .steals(teamStatsInGameData.getSteals())
-                .totReb(teamStatsInGameData.getTotReb())
-                .secondChancePoints(teamStatsInGameData.getSecondChancePoints())
-                .turnovers(teamStatsInGameData.getTurnovers())
-                .plusMinus(teamStatsInGameData.getPlusMinus())
-                .pointsInPaint(teamStatsInGameData.getPointsInPaint())
-                .pointsOffTurnovers(teamStatsInGameData.getPointsOffTurnovers())
-                .points(teamStatsInGameData.getPoints())
-                .build();
+        return NbaTeamGameStatisticsEntity.builder().team(nbaTeam).fga(teamStatsInGameData.getFga()).fgm(teamStatsInGameData.getFgm()).fgp(teamStatsInGameData.getFgp()).fta(teamStatsInGameData.getFta()).ftm(teamStatsInGameData.getFtm()).ftp(teamStatsInGameData.getFtp()).min(teamStatsInGameData.getMin()).assists(teamStatsInGameData.getAssists()).biggestLead(teamStatsInGameData.getBiggestLead()).fastBreakPoints(teamStatsInGameData.getFastBreakPoints()).blocks(teamStatsInGameData.getBlocks()).defReb(teamStatsInGameData.getDefReb()).offReb(teamStatsInGameData.getOffReb()).tpa(teamStatsInGameData.getTpa()).tpm(teamStatsInGameData.getTpm()).tpp(teamStatsInGameData.getTpp()).pFouls(teamStatsInGameData.getPFouls()).longestRun(teamStatsInGameData.getLongestRun()).steals(teamStatsInGameData.getSteals()).totReb(teamStatsInGameData.getTotReb()).secondChancePoints(teamStatsInGameData.getSecondChancePoints()).turnovers(teamStatsInGameData.getTurnovers()).plusMinus(teamStatsInGameData.getPlusMinus()).pointsInPaint(teamStatsInGameData.getPointsInPaint()).pointsOffTurnovers(teamStatsInGameData.getPointsOffTurnovers()).points(teamStatsInGameData.getPoints()).build();
+    }
+
+    public List<NbaGameStatisticsDto> injectAllTeamGameStatisticPerYear(Integer year, GameStatsResponse response) {
+        List<NbaGameDto> yearsGame = nbaGameInjectorService.getNbaGameBySeasonYear(year);
+        if (yearsGame == null || yearsGame.isEmpty()) {
+            log.error("No game statistic to inject - empty response");
+            return List.of();
+        }
+
+        List<NbaGameStatisticsDto> nbaGameStatisticsDtos = new ArrayList<>();
+
+        for (NbaGameDto nbaGameDto : yearsGame) {
+            NbaGameStatisticsDto nbaGameStatisticsDto = this.injectTeamGameStatistic(nbaGameDto.oldId(), response);
+            nbaGameStatisticsDtos.add(nbaGameStatisticsDto);
+        }
+
+        return nbaGameStatisticsDtos;
     }
 }
